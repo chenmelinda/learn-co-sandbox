5de028ea29c4319dc64c8d7e9df2ad462c2dcb2d
voluble-key-1260 <aiqiaochen21@gmail.com>
2018-10-16 23:12:02 +0000

    automatically backed up by learn

---
+++ week-5/index.js
@@ -0,0 +1,23 @@
+console.log('messages!')
+
+console.log(document.querySelector('#third-div'))
+console.log(document.querySelectorAll('div p'))
+
+
+
+//1. create element
+let pTag= document.createElement('p')
+
+//2. put text in the paragraph tag
+pTag.innerText = "Some text"
+
+// 3. put it on the page
+let thirdDiv = document.querySelector('#third-div')
+thirdDiv.appendChild(pTag)
+
+//method chaining
+let firstP = document.querySelector('div p').remove()
+
+let pTag = document.querySelector('div p')
+pTag.remove()
+
